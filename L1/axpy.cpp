2b0aa74c7be988c920c8ca05887e229b76449d6b
Ravenwater <theo@stillwater-sc.com>
2018-03-14 10:31:34 -0400

    Adding first basic structure

---
+++ L1/axpy.cpp
@@ -0,0 +1,35 @@
+// axpy.cpp: example program contrasting a BLAS L1 ?axpy routine between FLOAT and POSIT
+//
+// Copyright (C) 2017-2018 Stillwater Supercomputing, Inc.
+//
+// This file is part of the HPR-BLAS project, which is released under an MIT Open Source license.
+
+#include "stdafx.h"
+
+#include <vector>
+#include <hprblas>
+
+using namespace std;
+using namespace sw::unum;
+using namespace sw::blas;
+
+int main(int argc, char** argv)
+try {
+	const size_t nbits = 16;
+	const size_t es = 1;
+	const size_t vecSize = 32;
+
+	int nrOfFailedTestCases = 0;
+
+	cout << "AXPY is " << 1 << endl;
+
+	return (nrOfFailedTestCases > 0 ? EXIT_FAILURE : EXIT_SUCCESS);
+}
+catch (char const* msg) {
+	cerr << msg << endl;
+	return EXIT_FAILURE;
+}
+catch (...) {
+	cerr << "Caught unknown exception" << endl;
+	return EXIT_FAILURE;
+}
