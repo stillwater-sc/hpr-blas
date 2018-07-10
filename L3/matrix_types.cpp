9ceb10a2991230bc5d219d2d2321af79c1b3b361
Ravenwater <theo@stillwater-sc.com>
2018-07-10 18:53:56 -0400

    WIP: adding benchmarking placeholders for numerical error and performance

--- L3/matrix_types.cpp
+++ L3/matrix_types.cpp
@@ -3,6 +3,10 @@
 // Copyright (C) 2017-2018 Stillwater Supercomputing, Inc.
 //
 // This file is part of the HPR-BLAS project, which is released under an MIT Open Source license.
+
+// warning C4996: 'std::copy::_Unchecked_iterators::_Deprecate': Call to 'std::copy' with parameters that may be unsafe - this call relies on the caller to check that the passed values are correct.
+// \mtl4\boost/numeric/mtl/operation/update.hpp(159): warning C4244: 'argument': conversion from 'const double' to 'float', possible loss of data
+#pragma warning( disable : 4996 4244)
 #include "common.hpp"
 #include <hprblas>
 
