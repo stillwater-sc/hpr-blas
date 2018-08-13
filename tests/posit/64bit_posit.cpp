a9939863641b37ede26e6e1049ca2bff88d9479c
Ravenwater <theo@stillwater-sc.com>
2018-08-13 14:48:23 -0400

    WIP: bringing in arbitrary precision floats to have as capability for verification

---
+++ tests/posit/64bit_posit.cpp
@@ -0,0 +1,68 @@
+// 64bit_posit.cpp: Functionality tests for standard 64-bit posits
+//
+// Copyright (C) 2017-2018 Stillwater Supercomputing, Inc.
+//
+// This file is part of the universal numbers project, which is released under an MIT Open Source license.
+
+#include "common.hpp"
+// enable posit arithmetic exceptions
+#define POSIT_THROW_ARITHMETIC_EXCEPTION 1
+#include <posit>
+#include "../tests/test_helpers.hpp"
+#include "../tests/posit_test_helpers.hpp"
+
+/*
+Standard posit with nbits = 64 have es = 3 exponent bits.
+*/
+
+int main(int argc, char** argv)
+try {
+	using namespace std;
+	using namespace sw::unum;
+
+	const size_t RND_TEST_CASES = 0; // 100000;
+
+	const size_t nbits = 64;
+	const size_t es = 3;
+
+	int nrOfFailedTestCases = 0;
+	bool bReportIndividualTestCases = false;
+	std::string tag = " posit<64,3>";
+
+	cout << "Standard posit<64,3> configuration tests" << endl;
+
+	posit<nbits, es> p;
+	cout << spec_to_string(p) << endl << endl;
+
+	cout << "Arithmetic tests " << RND_TEST_CASES << " randoms each" << endl;
+	nrOfFailedTestCases += ReportTestResult(ValidateThroughRandoms<nbits, es>(tag, bReportIndividualTestCases, OPCODE_ADD, RND_TEST_CASES), tag, "addition      ");
+	nrOfFailedTestCases += ReportTestResult(ValidateThroughRandoms<nbits, es>(tag, bReportIndividualTestCases, OPCODE_SUB, RND_TEST_CASES), tag, "subtraction   ");
+	nrOfFailedTestCases += ReportTestResult(ValidateThroughRandoms<nbits, es>(tag, bReportIndividualTestCases, OPCODE_MUL, RND_TEST_CASES), tag, "multiplication");
+	nrOfFailedTestCases += ReportTestResult(ValidateThroughRandoms<nbits, es>(tag, bReportIndividualTestCases, OPCODE_DIV, RND_TEST_CASES), tag, "division      ");
+
+	return (nrOfFailedTestCases > 0 ? EXIT_FAILURE : EXIT_SUCCESS);
+}
+catch (char const* msg) {
+	std::cerr << msg << std::endl;
+	return EXIT_FAILURE;
+}
+catch (const posit_arithmetic_exception& err) {
+	std::cerr << "Uncaught posit arithmetic exception: " << err.what() << std::endl;
+	return EXIT_FAILURE;
+}
+catch (const quire_exception& err) {
+	std::cerr << "Uncaught quire exception: " << err.what() << std::endl;
+	return EXIT_FAILURE;
+}
+catch (const posit_internal_exception& err) {
+	std::cerr << "Uncaught posit internal exception: " << err.what() << std::endl;
+	return EXIT_FAILURE;
+}
+catch (const std::runtime_error& err) {
+	std::cerr << "Uncaught runtime exception: " << err.what() << std::endl;
+	return EXIT_FAILURE;
+}
+catch (...) {
+	std::cerr << "Caught unknown exception" << std::endl;
+	return EXIT_FAILURE;
+}
