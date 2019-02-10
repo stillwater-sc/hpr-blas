2c8496dc4ca6ae57505a57be9a1884c4c7680f63
Ravenwater <theo@stillwater-sc.com>
2019-02-10 13:28:29 -0500

    Detailed feedback to explain when TwoSum fails

--- applications/numerical/two_sum.cpp
+++ applications/numerical/two_sum.cpp
@@ -17,6 +17,46 @@ a + b = s + r
 
 */
 
+template<size_t nbits, size_t es>
+void ReportTwoSumError(std::string test_case, std::string op, const sw::unum::posit<nbits, es>& a, const sw::unum::posit<nbits, es>& b, const sw::unum::posit<nbits, es>& s, const sw::unum::posit<nbits, es>& r) {
+	std::cerr << test_case << " "
+		<< std::setw(nbits) << a.get()
+		<< " " << op << " "
+		<< std::setw(nbits) << b.get()
+		<< " != "
+		<< std::setw(nbits) << s.get()
+		<< " " << op << " "
+		<< std::setw(nbits) << r.get()
+		<< " instead it yielded "
+		<< std::setw(nbits) << (a + b).get()
+		<< " vs "
+		<< std::setw(nbits) << (s + r).get()
+		<< " : " << sw::unum::pretty_print(s + r)
+		<< std::endl;
+}
+
+template<typename Scalar>
+void GenerateTwoSumTestCase(const Scalar& a, const Scalar& b) {
+	constexpr size_t nbits = a.nbits;
+
+	Scalar s = a + b;
+	Scalar aApprox = s - b;
+	Scalar bApprox = s - aApprox;
+	Scalar aDiff = a - aApprox;
+	Scalar bDiff = b - bApprox;
+	Scalar r = aDiff + bDiff;
+	std::cout << "a                      : " << std::setw(nbits) << a.get()       << " : " << a << std::endl;
+	std::cout << "b                      : " << std::setw(nbits) << b.get()       << " : " << b << std::endl;
+	std::cout << "a + b                  : " << std::setw(nbits) << (a+b).get()   << " : " << (a + b) << std::endl;
+	std::cout << "s                      : " << std::setw(nbits) << s.get()       << " : " << s << std::endl;
+	std::cout << "r                      : " << std::setw(nbits) << r.get()       << " : " << r << std::endl;
+	std::cout << "s + r                  : " << std::setw(nbits) << (s + r).get() << " : " << (s + r) << std::endl;
+	std::cout << "aApprox = s - a        : " << std::setw(nbits) << aApprox.get() << " : " << aApprox << std::endl;
+	std::cout << "bApprox = s - aApprox  : " << std::setw(nbits) << bApprox.get() << " : " << bApprox << std::endl;
+	std::cout << "aDiff = a - aApprox    : " << std::setw(nbits) << aDiff.get()   << " : " << aDiff << std::endl;
+	std::cout << "bDiff = b - bApprox    : " << std::setw(nbits) << bDiff.get()   << " : " << bDiff << std::endl;
+}
+
 // enumerate all addition cases for a posit configuration: is within 10sec till about nbits = 14
 template<size_t nbits, size_t es>
 int ValidateTwoSum(std::string tag, bool bReportIndividualTestCases) {
@@ -24,7 +64,7 @@ int ValidateTwoSum(std::string tag, bool bReportIndividualTestCases) {
 	const size_t NR_POSITS = (size_t(1) << nbits);
 	int nrOfFailedTests = 0;
 	using Posit = sw::unum::posit<nbits, es>;
-	Posit pa, pb, psum, premainder, pref;
+	Posit pa, pb, ps, pr, psum, pref;
 	pair<Posit, Posit> s_and_r;
 	double da, db;
 	for (size_t i = 0; i < NR_POSITS; i++) {
@@ -35,14 +75,14 @@ int ValidateTwoSum(std::string tag, bool bReportIndividualTestCases) {
 			db = double(pb);
 
 			s_and_r = sw::unum::twoSum(pa, pb);
-			psum = s_and_r.first;
-			premainder = s_and_r.second;
-			pref = psum + premainder;
+			ps = s_and_r.first;
+			pr = s_and_r.second;
+			pref = ps + pr;
 			psum = pa + pb;
 
 			if (psum != pref) {
 				nrOfFailedTests++;
-				if (bReportIndividualTestCases)	ReportBinaryArithmeticError("FAIL", "+", pa, pb, pref, psum);
+				if (bReportIndividualTestCases)	ReportTwoSumError("FAIL", "+", pa, pb, ps, pr);
 			}
 			else {
 				//if (bReportIndividualTestCases) ReportBinaryArithmeticSuccess("PASS", "+", pa, pb, pref, psum);
@@ -53,14 +93,12 @@ int ValidateTwoSum(std::string tag, bool bReportIndividualTestCases) {
 	return nrOfFailedTests;
 }
 
+#define MANUAL_TEST 1
+
 int main(int argc, char** argv)
 try {
 	using namespace std;
 	using namespace sw::unum;
-
-	constexpr size_t nbits = 32;
-	constexpr size_t es = 2;
-	using Posit = posit<nbits,es>;
 
 	// print detailed bit-level computational intermediate results
 	bool verbose = false;
@@ -75,16 +113,33 @@ try {
 
 	cout << "Posit TwoSum validation" << endl;
 
-//	nrOfFailedTestCases += ReportTestResult(ValidateTwoSum<2, 0>(tag, bReportIndividualTestCases), "posit<2,0>", "twoSum");
+#ifdef MANUAL_TEST
 
-//	nrOfFailedTestCases += ReportTestResult(ValidateTwoSum<3, 0>(tag, bReportIndividualTestCases), "posit<3,0>", "twoSum");
-//	nrOfFailedTestCases += ReportTestResult(ValidateTwoSum<3, 1>(tag, bReportIndividualTestCases), "posit<3,1>", "twoSum");
+
+	constexpr size_t nbits = 8;
+	constexpr size_t es = 1;
+	using Posit = posit<nbits, es>;
+	Posit a, b;
+	a = b = minpos<nbits, es>();
+	GenerateTwoSumTestCase(a, b);
+
+//	nrOfFailedTestCases += ReportTestResult(ValidateTwoSum<8, 0>(tag, bReportIndividualTestCases), "posit<8,0>", "twoSum");
+	nrOfFailedTestCases += ReportTestResult(ValidateTwoSum<8, 1>(tag, bReportIndividualTestCases), "posit<8,1>", "twoSum");
+//	nrOfFailedTestCases += ReportTestResult(ValidateTwoSum<8, 2>(tag, bReportIndividualTestCases), "posit<8,2>", "twoSum");
+//	nrOfFailedTestCases += ReportTestResult(ValidateTwoSum<8, 3>(tag, bReportIndividualTestCases), "posit<8,3>", "twoSum");
+//	nrOfFailedTestCases += ReportTestResult(ValidateTwoSum<8, 4>(tag, bReportIndividualTestCases), "posit<8,4>", "twoSum");
+//	nrOfFailedTestCases += ReportTestResult(ValidateTwoSum<8, 5>(tag, bReportIndividualTestCases), "posit<8,5>", "twoSum");
+
+#else
+	nrOfFailedTestCases += ReportTestResult(ValidateTwoSum<2, 0>(tag, bReportIndividualTestCases), "posit<2,0>", "twoSum");
+
+	nrOfFailedTestCases += ReportTestResult(ValidateTwoSum<3, 0>(tag, bReportIndividualTestCases), "posit<3,0>", "twoSum");
+	nrOfFailedTestCases += ReportTestResult(ValidateTwoSum<3, 1>(tag, bReportIndividualTestCases), "posit<3,1>", "twoSum");
 
 	nrOfFailedTestCases += ReportTestResult(ValidateTwoSum<4, 0>(tag, bReportIndividualTestCases), "posit<4,0>", "twoSum");
 	nrOfFailedTestCases += ReportTestResult(ValidateTwoSum<4, 1>(tag, bReportIndividualTestCases), "posit<4,1>", "twoSum");
 	nrOfFailedTestCases += ReportTestResult(ValidateTwoSum<4, 2>(tag, bReportIndividualTestCases), "posit<4,2>", "twoSum");
 
-	/*
 	nrOfFailedTestCases += ReportTestResult(ValidateTwoSum<5, 0>(tag, bReportIndividualTestCases), "posit<5,0>", "twoSum");
 	nrOfFailedTestCases += ReportTestResult(ValidateTwoSum<5, 1>(tag, bReportIndividualTestCases), "posit<5,1>", "twoSum");
 	nrOfFailedTestCases += ReportTestResult(ValidateTwoSum<5, 2>(tag, bReportIndividualTestCases), "posit<5,2>", "twoSum");
@@ -95,7 +150,7 @@ try {
 	nrOfFailedTestCases += ReportTestResult(ValidateTwoSum<6, 2>(tag, bReportIndividualTestCases), "posit<6,2>", "twoSum");
 	nrOfFailedTestCases += ReportTestResult(ValidateTwoSum<6, 3>(tag, bReportIndividualTestCases), "posit<6,3>", "twoSum");
 	nrOfFailedTestCases += ReportTestResult(ValidateTwoSum<6, 4>(tag, bReportIndividualTestCases), "posit<6,4>", "twoSum");
-	*/
+#endif MANUAL_TEST
 
 	// restore the previous ostream precision
 	cout << setprecision(precision);
