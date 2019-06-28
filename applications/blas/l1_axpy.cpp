cb3d1d237c4054683ebdde0f9f160f1aea1a0d66
Ravenwater <theo@stillwater-sc.com>
2019-06-27 20:21:39 -0400

    receiving and integrating BLAS examples from Universal

---
+++ applications/blas/l1_axpy.cpp
@@ -0,0 +1,79 @@
+// l1_axpy.cpp: example program contrasting a BLAS L1 ?axpy routine between FLOAT and POSIT
+//
+// Copyright (C) 2017-2019 Stillwater Supercomputing, Inc.
+//
+// This file is part of the universal numbers project, which is released under an MIT Open Source license.
+#include "common.hpp"
+// enable posit arithmetic exceptions
+#define POSIT_THROW_ARITHMETIC_EXCEPTION 1
+#include <posit>
+
+// axpy: a times x plus y
+template<typename scale_T, typename vector_T>
+void axpy(size_t n, scale_T a, const vector_T& x, size_t incx, vector_T& y, size_t incy) {
+	size_t cnt, ix, iy;
+	for (cnt = 0, ix = 0, iy = 0; cnt < n && ix < x.size() && iy < y.size(); ++cnt, ix += incx, iy += incy) {
+		y[iy] += a * x[ix];
+	}
+}
+
+// print a vector
+template<typename vector_T>
+void printStridedVector(std::ostream& ostr, size_t n, vector_T& x, size_t incx = 1) {
+	size_t cnt, ix;
+	for (cnt = 0, ix = 0; cnt < n && ix < x.size(); ++cnt, ix += incx) {
+		cnt == 0 ? ostr << "[" << x[ix] : ostr << ", " << x[ix];
+	}
+	ostr << "]";
+}
+
+int main(int argc, char** argv)
+try {
+	using namespace std;
+	using namespace sw::unum;
+
+	constexpr size_t nbits = 16;
+	constexpr size_t es = 1;
+	//constexpr size_t vecSize = 32;
+
+	int nrOfFailedTestCases = 0;
+
+	constexpr int d = 5;
+	vector< posit<nbits, es> > v1 = { 1.0, 2.0, 3.0, 4.0, 5.0 };
+	vector< posit<nbits, es> > v2(d);
+	posit<nbits, es> alpha = minpos<nbits, es>();
+
+	cout << "AXPY is " << endl;
+	printStridedVector(cout, d, v1, 1); cout << endl;
+	printStridedVector(cout, d, v2, 1); cout << endl;
+
+	axpy(d, alpha, v1, 1, v2, 1);
+
+	printStridedVector(cout, d, v2, 1); cout << endl;
+
+	return (nrOfFailedTestCases > 0 ? EXIT_FAILURE : EXIT_SUCCESS);
+}
+catch (char const* msg) {
+	std::cerr << msg << std::endl;
+	return EXIT_FAILURE;
+}
+catch (const posit_arithmetic_exception& err) {
+	std::cerr << "Uncaught posit arithmetic exception: " << err.what() << std::endl;
+	return EXIT_FAILURE;
+}
+catch (const quire_exception& err) {
+	std::cerr << "Uncaught quire exception: " << err.what() << std::endl;
+	return EXIT_FAILURE;
+}
+catch (const posit_internal_exception& err) {
+	std::cerr << "Uncaught posit internal exception: " << err.what() << std::endl;
+	return EXIT_FAILURE;
+}
+catch (const std::runtime_error& err) {
+	std::cerr << "Uncaught runtime exception: " << err.what() << std::endl;
+	return EXIT_FAILURE;
+}
+catch (...) {
+	std::cerr << "Caught unknown exception" << std::endl;
+	return EXIT_FAILURE;
+}
