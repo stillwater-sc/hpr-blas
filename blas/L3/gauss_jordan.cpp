c381eb204d4cef47fb529f9029fc734984b015ce
Theodore Omtzigt <theodore.omtzigt@ibm.com>
2019-07-19 18:39:00 -0400

    experiments with numerical error measures

--- blas/L3/gauss_jordan.cpp
+++ blas/L3/gauss_jordan.cpp
@@ -4,7 +4,10 @@
 //
 // This file is part of the HPR-BLAS project, which is released under an MIT Open Source license.
 
-#include <chrono>
+//#include <chrono>
+// Boost arbitrary precision floats
+#include <boost/multiprecision/cpp_bin_float.hpp>
+
 // configure the posit number system behavior
 #define POSIT_ROUNDING_ERROR_FREE_IO_FORMAT 0
 // configure the HPR-BLAS behavior
@@ -13,73 +16,66 @@
 #include <mtl_extensions.hpp>
 // utilities to generate and print vectors and matrices
 #include "utils/matvec.hpp"
-#include "norms.hpp"
-#include "solvers/gauss_jordan.hpp"
 
-void dummy() {
+template<typename Scalar>
+void GenerateNumericalAnalysisTestCase(const std::string& header, unsigned N, bool verbose = false) {
 	using namespace std;
-    int i, j, k, n = 3;
-	float a[10][10] = { 
-		{ 2,3,4 },
-		{ 5,6,3 },
-		{ 9,8,6 }
-	};
-	float d;
-	
- 
-    for (i = 1; i <= n; i++)
-        for (j = 1; j <= 2 * n; j++)
-            if (j == (i + n))
-                a[i][j] = 1;
- 
-    /************** partial pivoting **************/
-    for (i = n; i > 1; i--)
-    {
-        if (a[i - 1][1] < a[i][1])
-            for (j = 1; j <= n * 2; j++)
-            {
-                d = a[i][j];
-                a[i][j] = a[i - 1][j];
-                a[i - 1][j] = d;
-            }
-    }
-    cout << "pivoted output: " << endl;
-    for (i = 1; i <= n; i++)
-    {
-        for (j = 1; j <= n * 2; j++)
-            cout << a[i][j] << "    ";
-        cout << endl;
-    }
-    /********** reducing to diagonal  matrix ***********/
- 
-    for (i = 1; i <= n; i++)
-    {
-        for (j = 1; j <= n * 2; j++)
-            if (j != i)
-            {
-                d = a[j][i] / a[i][i];
-                for (k = 1; k <= n * 2; k++)
-                    a[j][k] -= a[i][k] * d;
-            }
-    }
-    /************** reducing to unit matrix *************/
-    for (i = 1; i <= n; i++)
-    {
-        d = a[i][i];
-        for (j = 1; j <= n * 2; j++)
-            a[i][j] = a[i][j] / d;
-    }
- 
-    cout << "your solutions: " << endl;
-    for (i = 1; i <= n; i++)
-    {
-        for (j = n + 1; j <= n * 2; j++)
-            cout << a[i][j] << "    ";
-        cout << endl;
-    }
- 
+	using namespace mtl;
+	using namespace sw::unum;
+	using namespace sw::hprblas;
 
+	std::cout << ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n" << header << std::endl;
+	using Vector = mtl::vec::dense_vector<Scalar>;
+	using Matrix = mtl::mat::dense2D<Scalar>;
 
+	Matrix H(N, N);
+	GenerateHilbertMatrix(H, false);
+	Matrix Hinv = GaussJordanInversion(H);
+	Matrix Href(N, N);
+	GenerateHilbertMatrixInverse(Href);
+	Matrix test(N, N);
+	matmul(test, H, Hinv);
+	if (verbose) {
+		printMatrix(cout, "Hilbert matrix order 5", H);
+		printMatrix(cout, "Hilbert inverse", Hinv);
+
+		printMatrix(cout, "Hilbert inverse reference", Href);
+
+		printMatrix(cout, "H * H^-1", test);
+	}
+
+	Matrix I(N, N);
+	matmul(I, H, Hinv);  // should yield the identity matrix
+
+	// calculate the numerical error caused by the linear algebra computation
+	Vector e(N), eprime(N), eabsolute(N), erelative(N);
+	e = Scalar(1);
+	matvec(I, e, eprime);
+	printVector(cout, "reference vector", e);
+	printVector(cout, "error vector", eprime);
+	// absolute error
+	eabsolute = e - eprime;
+	printVector(cout, "absolute error vector", eabsolute);
+	cout << "L1 norm   " << posit_format(l1_norm(eabsolute)) << "  " << l1_norm(eabsolute) << endl;
+	cout << "L2 norm   " << posit_format(l2_norm(eabsolute)) << "  " << l2_norm(eabsolute) << endl;
+	cout << "Linf norm " << posit_format(linf_norm(eabsolute)) << "  " << linf_norm(eabsolute) << endl;
+
+	// relative error
+	cout << "relative error\n";
+	Scalar relative_error;
+	relative_error = l1_norm(eabsolute) / l1_norm(e);
+	cout << "L1 norm   " << posit_format(relative_error) << "  " << relative_error << endl;
+	relative_error = l2_norm(eabsolute) / l2_norm(e);
+	cout << "L2 norm   " << posit_format(relative_error) << "  " << relative_error << endl;
+	relative_error = linf_norm(eabsolute) / linf_norm(e);
+	cout << "Linf norm " << posit_format(relative_error) << "  " << relative_error << endl;
+
+	// error volume
+	cout << "error bounding box volume\n";
+	cout << "Measured in Euclidean distance    : " << error_volume(linf_norm(eabsolute), N, false) << endl;
+	cout << "Measured in ULPs                  : " << error_volume(linf_norm(eabsolute), N, true) << " ulps^" << N << endl;
+	Scalar ulp = numeric_limits<Scalar>::epsilon();
+	cout << "L-infinitiy norm measured in ULPs : " << linf_norm(eabsolute) / ulp << " ulps" << endl;
 }
 
 int main(int argc, char** argv)
@@ -88,46 +84,27 @@ try {
 	using namespace mtl;
 	using namespace sw::unum;
 	using namespace sw::hprblas;
+	using namespace boost::multiprecision;
 
+	using sp = boost::multiprecision::cpp_bin_float_single;
+	using dp = boost::multiprecision::cpp_bin_float_double;
+	using qp = boost::multiprecision::cpp_bin_float_quad;
 	constexpr size_t nbits = 64;
 	constexpr size_t es = 3;
 	constexpr size_t capacity = 10;
 
-	using Scalar = posit<nbits, es>;
-	using Vector = mtl::vec::dense_vector<Scalar>;
-	using Matrix = mtl::mat::dense2D<Scalar>;
-	size_t N = 5;
-	Matrix H(N, N);
-	GenerateHilbertMatrix(H);
-	Matrix Hinv = GaussJordanInversion(H);
-	printMatrix(cout, "Hilbert matrix order 5", H);
-	printMatrix(cout, "Hilbert inverse", Hinv);
-	Matrix Href(N, N);
-	GenerateHilbertMatrixInverse(Href);
-	printMatrix(cout, "Hilbert inverse reference", Href);
-
-	Matrix test(N, N);
-	matmul(test, H, Hinv);
-	printMatrix(cout, "H * H^-1", test);
-
-	Matrix I(N, N);
-	matmul(I, H, Hinv);
-	Vector e(N), eprime(N), erelative(N);
-	e = Scalar(1);
-	matvec(I, e, eprime);
-	cout << "L1 norm   " << posit_format(l1_norm(eprime)) << "  " << l1_norm(eprime) << endl;
-	cout << "L2 norm   " << posit_format(l2_norm(eprime)) << "  " << l2_norm(eprime) << endl;
-	cout << "Linf norm " << posit_format(linf_norm(eprime)) << "  " << linf_norm(eprime) << endl;
-	printVector(cout, "reference vector", e);
-	printVector(cout, "error vector", eprime);
-
-	// relative error
-	erelative = e - eprime;
-	printVector(cout, "relative error", erelative);
-	cout << "L1 norm   " << posit_format(l1_norm(erelative)) << "  " << l1_norm(erelative) << endl;
-	cout << "L2 norm   " << posit_format(l2_norm(erelative)) << "  " << l2_norm(erelative) << endl;
-	cout << "Linf norm " << posit_format(linf_norm(erelative)) << "  " << linf_norm(erelative) << endl;
-
+	unsigned N = 5; 
+	GenerateNumericalAnalysisTestCase< posit<32, 2> >("posit<32,2>", N);
+	cout << endl;
+	GenerateNumericalAnalysisTestCase< float >("IEEE single precision", N);
+	cout << endl;
+	GenerateNumericalAnalysisTestCase< posit<64, 3> >("posit<64,3>", N);
+	cout << endl;
+	GenerateNumericalAnalysisTestCase< double >("IEEE double precision", N);
+	cout << endl;
+	GenerateNumericalAnalysisTestCase< posit<128, 4> >("posit<128,4>", N);
+	cout << endl;
+	GenerateNumericalAnalysisTestCase< qp >("IEEE quad precision", N);
 
 	return EXIT_SUCCESS;
 }
