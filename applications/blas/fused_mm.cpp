6cbc2243b4eb7eb0f19af79e7b02096c65fa48e5
Ravenwater <theo@stillwater-sc.com>
2019-08-17 11:31:23 -0400

    WIP: initial implementation of a parameterized block-based fused matrix multiply

--- applications/blas/fused_mm.cpp
+++ applications/blas/fused_mm.cpp
@@ -34,27 +34,31 @@ try {
 	// configure the number system
 	constexpr size_t nbits = 32;
 	constexpr size_t es = 2;
-	using Scalar = posit<nbits, es>;
+	using Scalar = typename sw::unum::posit<nbits, es>;
+	using Matrix = typename mtl::mat::dense2D<Scalar>;
+	constexpr size_t blockHeight = 5;
+	constexpr size_t blockWidth = 5;
 
-	size_t N = 5;
-	mtl::mat::dense2D<Scalar> H(N, N);
+	size_t N = 10;
+	Matrix H(N, N);
 	GenerateHilbertMatrix(H);
 
-	mtl::mat::dense2D<Scalar> Hinv(N, N);
+	Matrix Hinv(N, N);
 	GenerateHilbertMatrixInverse(Hinv);
 
-	mtl::mat::dense2D<Scalar> I(N, N);
-	I = sw::hprblas::fmm(H, Hinv);
+	Matrix I1(N, N), I2(N, N);
+	I1 = sw::hprblas::fmm(H, Hinv);
+	I2 = sw::hprblas::bfmm<Matrix, blockHeight, blockWidth>(H, Hinv);
 
 	Scalar lcm = HilbertScalingFactor(N);
-	I = I / lcm;
+	I1 = I1 / lcm;
 
-	printMatrix(cout, "H * H^-1", I);
+	printMatrix(cout, "H * H^-1", I1);
 
 	mtl::mat::dense2D<Scalar> eye(N, N);
 	eye = Scalar(1);
 
-	if (!isEqual(I, eye)) ++nrOfFailedTestCases;
+	if (!isEqual(I1, eye)) ++nrOfFailedTestCases;
 
 	if (nrOfFailedTestCases) cout << "FAIL" << endl; else cout << "PASS" << endl;
 
--- blas/L3/matmul_morton.cpp
+++ blas/L3/matmul_morton.cpp
@@ -11,20 +11,25 @@
 // utilities to generate and print vectors and matrices
 #include "utils/matvec.hpp"
 
-int morton_recursion()
+template<typename Scalar, size_t n = 10>
+int MortonMatrixExamples()
 {
 	using namespace mtl; 
 	using namespace mtl::mat;
 
-	const unsigned n = 20;
-	dense2D<double>                            A(n, n), B(n, n);
-	morton_dense<double, doppled_64_row_mask>  C(n, n);
+	dense2D<Scalar>                            A(n, n), B(n, n);
+	morton_dense<Scalar, doppled_64_row_mask>  C(n, n);
 
-	hessian_setup(A, 3.0); hessian_setup(B, 1.0);
+	hessian_setup(A, 3.0); 
+	hessian_setup(B, 1.0);
 	hessian_setup(C, 2.0);
+
+	std::cout << A << std::endl;
 
 	// Corresponds to A= B * B;
 	mult(B, B, A);
+
+	std::cout << A << std::endl;
 
 	A = B * B;   // use BLAS
 	A = B * C;   // use recursion + tiling from MTL4
@@ -35,39 +40,18 @@ int morton_recursion()
 	return 0;
 }
 
-
-int main(int argc, char** argv)
-try {
+template<typename Scalar>
+void BenchmarkMorton() {
 	using namespace std;
-	using namespace sw::unum;
-	using namespace sw::hprblas;
+	using namespace chrono;
 	using namespace mtl;
-
-	// a 32-bit float and a <27,1> posit have the same number of significand bits around 1.0
-	constexpr size_t nbits = 27;
-	constexpr size_t es = 2;
-	constexpr size_t capacity = 10;
-
-	typedef float            IEEEType;
-	typedef posit<nbits, es> PositType;
-	cout << dynamic_range(posit<nbits, es>()) << endl;
-
-	float eps = std::numeric_limits<IEEEType>::epsilon();
-	float epsminus = 1.0f - eps;
-	float epsplus = 1.0f + eps;
-	float max_fexp = std::numeric_limits<IEEEType>::max_exponent;
-	float min_fexp = std::numeric_limits<IEEEType>::min_exponent;
-	cout << "IEEE float: epsilon " << eps << " min exp " << min_fexp << " max exp " << max_fexp << endl;
-
-	using namespace std::chrono;
-	for (int dim = 16; dim < 1025; dim *= 2) {
+	for (int dim = 16; dim < 511; dim *= 2) {
 		cout << "Matrix dimensions are: " << dim << " x " << dim << endl;
 
 		steady_clock::time_point t1 = steady_clock::now();
-		mtl::dense2D<IEEEType> A(dim, dim), B(dim, dim);
-		morton_dense<double, doppled_64_row_mask>  C(dim, dim);
+		morton_dense<Scalar, doppled_64_row_mask>  A(dim, dim), B(dim, dim), C(dim, dim);
 		steady_clock::time_point t2 = steady_clock::now();
-		duration<double> time_span = duration_cast< duration<double> >(t2 - t1);
+		duration<double> time_span = duration_cast<duration<double>>(t2 - t1);
 		double elapsed = time_span.count();
 		cout << "  Construction     " << elapsed << " seconds.\n";
 
@@ -75,7 +59,7 @@ try {
 		sw::hprblas::uniform_rand(A);
 		sw::hprblas::uniform_rand(B);
 		t2 = steady_clock::now();
-		time_span = duration_cast< duration<double> >(t2 - t1);
+		time_span = duration_cast<duration<double>>(t2 - t1);
 		elapsed = time_span.count();
 		cout << "  Random fill      " << elapsed << " seconds.\n";
 
@@ -91,6 +75,34 @@ try {
 		double flops = double(N)*dim*dim*dim / elapsed * 1.0e-9;
 		cout << "  Performance:     " << flops << " GFLOPS Single Precision\n";
 	}
+}
+
+template<typename Scalar>
+void NumericalAttributes() {
+	Scalar eps = std::numeric_limits<Scalar>::epsilon();
+	Scalar epsminus = Scalar(1.0) - eps;
+	Scalar epsplus = Scalar(1.0) + eps;
+	Scalar max_exp = std::numeric_limits<Scalar>::max_exponent;
+	Scalar min_exp = std::numeric_limits<Scalar>::min_exponent;
+	cout << " epsilon " << eps << " min exp " << min_exp << " max exp " << max_exp << endl;
+}
+
+int main(int argc, char** argv)
+try {
+	using namespace std;
+	using namespace sw::unum;
+	using namespace sw::hprblas;
+	using namespace mtl;
+
+	// a 32-bit float and a <27,1> posit have the same number of significand bits around 1.0
+	constexpr size_t nbits = 27;
+	constexpr size_t es = 2;
+	constexpr size_t capacity = 10;
+
+	using Posit = posit<32, 2>;
+
+	MortonMatrixExamples<float>();
+//	BenchmarkMorton<Posit>();
 
 	return EXIT_SUCCESS;
 }
