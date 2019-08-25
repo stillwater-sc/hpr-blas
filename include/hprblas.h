3547031337d9392ba78b5250531562d08249f1bf
Ravenwater <theo@stillwater-sc.com>
2019-08-25 15:28:02 -0400

    WIP: creating a C api to the fused dot, matvec, and matmul algorithms

---
+++ include/hprblas.h
@@ -0,0 +1,27 @@
+#pragma once
+// hprblas.h: generic C API bindings for the HPRBLAS API
+// 
+// Copyright (C) 2017-2019 Stillwater Supercomputing, Inc.
+//
+// This file is part of the HPRBLAS project, which is released under an MIT Open Source license.
+
+
+// set up the correct C11 infrastructure
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdint.h>
+
+// standard posit C types
+#include <positctypes.h>
+#include <posit_c_api.h>
+
+#ifdef __cplusplus
+// export a C interface if used by C++ source code
+extern "C" {
+#endif
+
+
+#ifdef __cplusplus
+}
+#endif
