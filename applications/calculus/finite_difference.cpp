67b2ede9fbd1dd3788a8760082571811f796f37a
Ravenwater <theo@stillwater-sc.com>
2019-11-05 07:20:16 -0500

    renaming left/right to backward/forward and adding central difference

--- applications/calculus/finite_difference.cpp
+++ applications/calculus/finite_difference.cpp
@@ -14,9 +14,9 @@
 
    */
 template <typename F, typename T>
-class right_derivative {
+class forward_difference {
 public:
-	right_derivative(const F& f, const T& h) : f(f), h(h) {}
+	forward_difference(const F& f, const T& h) : f(f), h(h) {}
 	T operator()(const T& x) const { return (f(x+h) - f(x)) / h; }
 private:
 	const F& f;
@@ -24,10 +24,20 @@ private:
 };
 
 template<typename F, typename T>
-class left_derivative {
+class backward_difference {
 public:
-	left_derivative(const F& f, const T& h) : f(f), h(h) {}
+	backward_difference(const F& f, const T& h) : f(f), h(h) {}
 	T operator()(const T& x) const { return (f(x) - f(x - h)) / h; }
+private:
+	const F& f;
+	T        h;
+};
+
+template<typename F, typename T>
+class central_difference {
+public:
+	central_difference(const F& f, const T& h) : f(f), h(h) {}
+	T operator()(const T& x) const { return (f(x + T(0.5)*h) - f(x - T(0.5)*h)) / h; }
 private:
 	const F& f;
 	T        h;
@@ -48,7 +58,7 @@ private:
 
 // termination condition
 template<typename F, typename T>
-class nth_derivative<1, F, T> : public right_derivative<F, T> {
+class nth_derivative<1, F, T> : public forward_difference<F, T> {
 	//using derivative<F, T>::derivative;
 };
 
@@ -58,7 +68,7 @@ struct sin_f {
 	T operator() (const T& x) const { return sin(x); }
 };
 
-// a functor with a scale parameter to trigger ADL type resolution
+// a functor with a scale parameter
 template<typename T>
 class scaled_sin_f {
 public:
@@ -89,24 +99,32 @@ void EnumerateDerivativeError() {
 	cout << "cos at PI/4 : " << cos_f<Real>()(m_pi_4) << endl;
 	cout << "tan at PI/4 : " << tan_f<Real>()(m_pi_4) << endl;
 
-	using rd_sin_f = right_derivative<sin_f<Real>, Real>;
-	using ld_sin_f = left_derivative<sin_f<Real>, Real>;
+	using forward_sin_f = forward_difference<sin_f<Real>, Real>;
+	using backward_sin_f = backward_difference<sin_f<Real>, Real>;
+	using central_sin_f = central_difference<sin_f<Real>, Real>;
 
 	constexpr int WIDTH = 16;
 
 	Real h = Real(0.05);
 	sin_f<Real> sin_o;
-	cout << setw(WIDTH) << "step (h)" << setw(WIDTH) << "left" << setw(WIDTH) << "error" << setw(WIDTH) << "right" << setw(WIDTH) << "error\n";
+	cout << setw(WIDTH) << "step (h)" << setw(WIDTH) << "backward" << setw(WIDTH) << "error" << setw(WIDTH) << "forward" << setw(WIDTH) << "error" << setw(WIDTH) << "central" << setw(WIDTH) << "error\n";
 	for (unsigned i = 0; i < 10; ++i) {
-		ld_sin_f ld_sin_o(sin_o, h);
+		// backward = left
+		backward_sin_f ld_sin_o(sin_o, h);
 		Real ld_sin = ld_sin_o(Real(m_pi_4));
 		Real lerror = ld_sin - cos(m_pi_4);
 
-		rd_sin_f rd_sin_o(sin_o, h);
+		// forward = right
+		forward_sin_f rd_sin_o(sin_o, h);
 		Real rd_sin = rd_sin_o(Real(m_pi_4));
 		Real rerror = rd_sin - cos(m_pi_4);
 
-		cout << setw(WIDTH) << h << setw(WIDTH) << ld_sin << setw(WIDTH) << lerror << setw(WIDTH) << rd_sin << setw(WIDTH) << rerror << endl;
+		// central = middle
+		central_sin_f md_sin_o(sin_o, h);
+		Real md_sin = md_sin_o(Real(m_pi_4));
+		Real merror = md_sin - cos(m_pi_4);
+
+		cout << setw(WIDTH) << h << setw(WIDTH) << ld_sin << setw(WIDTH) << lerror << setw(WIDTH) << rd_sin << setw(WIDTH) << rerror << setw(WIDTH) << md_sin << setw(WIDTH) << merror << endl;
 
 		h *= 0.5;
 	}
