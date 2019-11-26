9cf5c04bbcfdae79e3300c36c321e66880c747d2
Ravenwater <theo@stillwater-sc.com>
2019-11-26 18:35:33 -0500

    consolidating the Cholesky solvers into their solver placeholder

--- applications/blas/cholesky.cpp
+++ applications/blas/cholesky.cpp
@@ -5,266 +5,6 @@
 // This file is part of the HPR-BLAS project, which is released under an MIT Open Source license.
 #include <hprblas>
 
-// In-place Cholesky factorization of an SPD matrix A, generating the factor UT and the diagonal
-template<typename Matrix, typename Vector>
-bool CholeskyFactorization(Matrix& A, Vector& diagonal) {
-	using Scalar = typename Matrix::value_type;
-	int N = int(mtl::mat::num_cols(A));
-
-	for (int i = 0; i < N; ++i) {
-		for (int j = i; j < N; ++j) {
-			Scalar sum = A[i][j];
-			for (int k = i - 1; k >= 0; k--) {
-				sum -= A[i][k] * A[j][k];
-			}
-			if (i == j) {
-				if (sum <= 0) {
-					std::cerr << "Matrix is not positive definite!\n";
-					return false;
-				}
-				diagonal[i] = sqrt(sum);
-			}
-			else {
-				A[j][i] = sum / diagonal[i];
-			}
-		}
-	}
-	return true;
-}
-
-// Cholesky factorization of an SPD matrix A, generating the factor UT and the diagonal
-template<typename Matrix, typename Vector>
-bool CholeskyFactorization(const Matrix& A, Matrix& UT, Vector& diagonal) {
-	using Scalar = typename Matrix::value_type;
-	int N = int(mtl::mat::num_cols(A));
-
-	UT = A;
-	for (int i = 0; i < N; ++i) {
-		for (int j = i; j < N; ++j) {
-			Scalar sum = A[i][j];
-			for (int k = i - 1; k >= 0; k--) {
-				sum -= UT[i][k] * UT[j][k];
-			}
-			if (i == j) {
-				if (sum <= 0) {
-					std::cout << "Matrix is not positive definite!\n";
-					return false;
-				}
-				diagonal[i] = sqrt(sum);
-			}
-			else {
-				UT[j][i] = sum / diagonal[i];
-			}
-		}
-	}
-	return true;
-}
-
-// CholeskyDecomposition takes a Symmetric Positive Definite Matrix A, and returns the lower triangular Cholesky factorized matrix U transpose (UT)
-template<typename Matrix>
-bool CholeskyDecomposition(const Matrix& A, Matrix& UT) {
-	assert(mtl::mat::num_rows(A) == mtl::mat::num_cols(A)); // assert squareness
-	using Scalar = typename Matrix::value_type;
-	int N = int(mtl::mat::num_cols(A));
-	mtl::vec::dense_vector<Scalar> diagonal(N);
-
-	bool success = CholeskyFactorization(A, UT, diagonal);
-	if (!success) return false;
-
-	// complete the UT by nulling out the upper triangular part
-	for (int i = 0; i < N; ++i) {
-		UT[i][i] = diagonal[i];
-		for (int j = i + 1; j < N; ++j) {
-			UT[i][j] = 0;
-		}
-	}
-
-	return true;
-}
-
-// CholeskyDecomposition takes a Symmetric Positive Definite Matrix A, and returns the lower triangular Cholesky factorized matrix U transpose (UT)
-template<typename Matrix>
-Matrix CholeskyDecomposition(const Matrix& A) {
-	using Scalar = typename Matrix::value_type;
-	int N = int(mtl::mat::num_cols(A));
-	Matrix UT(N, N);
-	mtl::vec::dense_vector<Scalar> diagonal(N);
-
-	UT = 0;
-	bool success = CholeskyFactorization(A, UT, diagonal);
-	if (!success) {
-		UT = 0;
-		return UT;
-	}
-
-	// complete the UT by nulling out the upper triangular part
-	for (int i = 0; i < N; ++i) {
-		UT[i][i] = diagonal[i];
-		for (int j = i + 1; j < N; ++j) {
-			UT[i][j] = 0;
-		}
-	}
-	return UT;
-}
- 
-/* -----------------------------------------------------
-         Inverse of Cholesky decomposition.
-
-         input    A     Symmetric positive def. matrix
-         output   Linv  inverse of lower decomposed matrix
-         uses        choldc1(Matrix,Vector)         
-   ----------------------------------------------------- */
-template<typename Matrix>
-void choldcsl(const Matrix& A, Matrix& Linv) {
-	using Scalar = typename Matrix::value_type;
-	int N = int(mtl::mat::num_cols(A));
-	for (int i = 0; i < N; ++i) {
-		for (int j = 0; j < N; ++j) {
-			Linv[i][j] = A[i][j];
-		}
-	}
-
-	mtl::vec::dense_vector<Scalar> p(N);
-	CholeskyFactorization(Linv, p);
-	for (int i = 0; i < N; ++i) {
-		Linv[i][i] = 1 / p[i];
-		for (int j = i + 1; j < N; ++j) {
-			Scalar sum = Scalar(0);
-			for (int k = i; k < j; ++k) {
-				sum -= Linv[j][k] * Linv[k][i];
-			}
-			Linv[j][i] = sum / p[j];
-		}
-	}
-}
-
-// Return value of the determinant of a symmetric, positive definite matrix via a Cholesky decomposition
-template<typename Matrix>
-typename Matrix::value_type DeterminantSPD(const Matrix& A) {	
-	int N = int(mtl::mat::num_cols(A));
-	Matrix C(N, N);
-	CholeskyDecomposition(A, C);
-	using Scalar = typename Matrix::value_type;
-	Scalar d = Scalar(1);
-	for (int i = 0; i < N; ++i) {
-		d *= C[i][i];
-	}
-	return d * d;
-}
- 
-/* ---------------------------------------------------
-        Matrix inverse using Cholesky decomposition
-
-        input	  A  Symmetric positive def. matrix
-        output   Ainv  inverse of A
-        uses        choldc1(MAT, VEC)
-   --------------------------------------------------- */
-template<typename Matrix>
-void cholsl(const Matrix& A, Matrix& Ainv) {
-	choldcsl(A, Ainv);
-//	std::cout << "first   Ainv\n" << Ainv << std::endl;
-
-	int N = int(mtl::mat::num_cols(A));
-	for (int i = 0; i < N; ++i) {
-		for (int j = i + 1; j < N; ++j) {
-			Ainv[i][j] = 0.0;
-		}
-	}
-//	std::cout << "second   Ainv\n" << Ainv << std::endl;
-
-	for (int i = 0; i < N; ++i) {
-		Ainv[i][i] *= Ainv[i][i];
-		for (int k = i + 1; k < N; ++k) {
-			Ainv[i][i] += Ainv[k][i] * Ainv[k][i];
-		}
-		for (int j = i + 1; j < N; ++j) {
-			for (int k = j; k < N; ++k) {
-				Ainv[i][j] += Ainv[k][i] * Ainv[k][j];
-			}
-		}
-	}
-//	std::cout << "third  Ainv\n" << Ainv << std::endl;
-
-	for (int i = 0; i < N; ++i) {
-		for (int j = 0; j < i; ++j) {
-			Ainv[i][j] = Ainv[j][i];
-		}
-	}
-//	std::cout << "final  Ainv\n" << Ainv << std::endl;
-
-}
-
-template<typename Matrix>
-Matrix Inverse(const Matrix& A) {
-	assert(mtl::mat::num_rows(A) == mtl::mat::num_cols(A)); // assert squareness
-	using Scalar = Matrix::value_type;
-	int N = int(mtl::mat::num_cols(A));
-	mtl::vec::dense_vector<Scalar> diagonal(N);	
-	Matrix Ainv(N, N);
-	Ainv = A;
-	for (int i = 0; i < N; ++i) {
-		for (int j = i; j < N; ++j) {
-			Scalar sum = Ainv[i][j];
-			for (int k = i - 1; k >= 0; k--) {
-				sum -= Ainv[i][k] * Ainv[j][k];
-			}
-			if (i == j) {
-				if (sum <= 0) {
-					std::cerr << "inverse: matrix is not positive definite!\n";
-					Ainv = 0;
-					return Ainv;
-				}
-				diagonal[i] = sqrt(sum);
-			}
-			else {
-				Ainv[j][i] = sum / diagonal[i];
-			}
-		}
-	}
-	std::cout << "step 1\n" << Ainv << std::endl;
-
-	for (int i = 0; i < N; ++i) {
-		Ainv[i][i] = 1 / diagonal[i];
-		for (int j = i + 1; j < N; ++j) {
-			Scalar sum = Scalar(0);
-			for (int k = i; k < j; ++k) {
-				sum -= Ainv[j][k] * Ainv[k][i];
-			}
-			Ainv[j][i] = sum / diagonal[j];
-		}
-	}
-	std::cout << "step 2\n" << Ainv << std::endl;
-
-	for (int i = 0; i < N; ++i) {
-		for (int j = i + 1; j < N; ++j) {
-			Ainv[i][j] = 0.0;
-		}
-	}
-	std::cout << "step 3\n" << Ainv << std::endl;
-
-	for (int i = 0; i < N; ++i) {
-		Ainv[i][i] *= Ainv[i][i];
-		for (int k = i + 1; k < N; ++k) {
-			Ainv[i][i] += Ainv[k][i] * Ainv[k][i];
-		}
-		for (int j = i + 1; j < N; ++j) {
-			for (int k = j; k < N; ++k) {
-				Ainv[i][j] += Ainv[k][i] * Ainv[k][j];
-			}
-		}
-	}
-	std::cout << "step 4\n" << Ainv << std::endl;
-
-	for (int i = 0; i < N; ++i) {
-		for (int j = 0; j < i; ++j) {
-			Ainv[i][j] = Ainv[j][i];
-		}
-	}
-	std::cout << "step 5\n" << Ainv << std::endl;
-
-	return Ainv;
-}
-
 // CheckPositiveDefinite returns true if the Matrix A is a Positive Definite matrix
 template<typename Matrix>
 bool CheckPositiveDefinite(Matrix& A) {
@@ -330,6 +70,7 @@ void SetupMatrix(Matrix& A, int bandwidth = 0) {
 		}
 	}
 }
+
 // main program to demonstrate the use of function cholsl()
 int main(int argc, char* argv[]) 
 try {
@@ -361,7 +102,7 @@ try {
 		return EXIT_FAILURE;
 	}
 
-	Scalar determinant = DeterminantSPD(A);
+	Scalar determinant = sw::hprblas::DeterminantSPD(A);
 	cout << "Determinant = " << determinant << endl;
 
 	/*
@@ -408,12 +149,11 @@ try {
 	}
 	*/
 	Matrix Ainv(N, N);
-	Ainv = Inverse(A);
+	Ainv = sw::hprblas::Inverse(A);
 	cout << "Matrix Inv(A)  :\n" << Ainv << endl;
 
 	T = A * Ainv;
 	cout << "Verification A * Inv(A) = I:\n" << with_format(T, 10, 3) << endl;
-
 
 	{
 		// non-quire norms
