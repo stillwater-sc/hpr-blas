0a373d33bce3c1b79fb5e6b16f0694d490bc5f69
Ravenwater <theo@stillwater-sc.com>
2019-12-26 12:38:09 -0500

    fixing cmake files

---
+++ HPR_BLASConfig.cmake
@@ -0,0 +1,6 @@
+set(HPR_BLAS_COMMON_CONFIG "${HPR_BLAS_DIR}/tools/cmake/HPR_BLASCommon.cmake")
+if( EXISTS ${HPR_BLAS_COMMON_CONFIG})
+	include(${HPR_BLAS_COMMON_CONFIG})
+else()
+	message(ERROR "  Could not find the HPR-BLAS library configuration at ${HPR_BLAS_COMMON_CONFIG}")
+endif()
--- tools/cmake/HPR_BLASCommon.cmake
+++ tools/cmake/HPR_BLASCommon.cmake
@@ -1,10 +1,11 @@
 ############################
-#This configuration file defines some cmake variables:
-#HPR_BLAS_INCLUDE_DIRS: list of include directories for the universal library
-#HPR_BLAS_LIBRARIES: libraries needed for interfaces like umfpack and arprec, see below
-#HPR_BLAS_CXX_DEFINITIONS: definitions to enable the requested interfaces
-#HPR_BLAS_VERSION: version (current: 1)
-#HPR_BLAS_MINOR_VERSION: minor version 
+# This configuration file defines some cmake variables:
+# HPR_BLAS_INCLUDE_DIRS: list of include directories for the HPR-BLAS library
+# HPR_BLAS_LIBRARIES: libraries needed 
+# HPR_BLAS_CXX_DEFINITIONS: definitions to enable the requested interfaces
+# HPR_BLAS_VERSION: version
+# HPR_BLAS_MAJOR_VERSION: major version 
+# HPR_BLAS_MINOR_VERSION: minor version 
 #
 #supported components:
 #
@@ -12,6 +13,10 @@
 unset(HPR_BLAS_LIBRARIES )
 unset(HPR_BLAS_CXX_DEFINITIONS )
 unset(HPR_BLAS_INCLUDE_DIRS )
+
+set(HPR_BLAS_VERSION 1)
+set(HPR_BLAS_MAJOR_VERSION 0)
+set(HPR_BLAS_MINOR_VERSION 1)
 
 if (MSVC)
     add_definitions(/wd4522) # multiple assignment ops for single type, to be investigated further if avoidable
@@ -21,11 +26,17 @@ if (USE_ASSERTS)
   list(APPEND HPR_BLAS_CXX_DEFINITIONS "-DHPR_BLAS_ASSERT_FOR_THROW")
 endif()
 
-if(EXISTS ${HPR_BLAS_DIR}/blas/blas.hpp)
-	list(APPEND HPR_BLAS_INCLUDE_DIRS "${HPR_BLAS_DIR}/blas")
+if(EXISTS ${HPR_BLAS_DIR}/include/hprblas.hpp)
+	list(APPEND HPR_BLAS_INCLUDE_DIRS "${HPR_BLAS_DIR}/include")
 else()
-	list(APPEND HPR_BLAS_INCLUDE_DIRS "${HPR_BLAS_DIR}/../../include")
-endif(EXISTS ${HPR_BLAS_DIR}/blas/blas.hpp)
+	message(ERROR " HPR_BLAS was not found")
+	# assuming a dev tree where the repos are stored along side each other, i.e.
+	# .../dev/clones/universal
+	# .../dev/clones/mtl4
+	# .../dev/clones/hpr-blas
+	# .../dev/clones/hpr-dsp
+	list(APPEND HPR_BLAS_INCLUDE_DIRS "${HPR_BLAS_DIR}/../hpr-blas/include")
+endif(EXISTS ${HPR_BLAS_DIR}/include/hprblas.hpp)
 
 macro(hpr_blas_check_cxx_compiler_flag FLAG RESULT)
   # counts entirely on compiler's return code, maybe better to combine it with check_cxx_compiler_flag
