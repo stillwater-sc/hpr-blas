b199235626681273384dd316f85d8600232917bc
Ravenwater <theo@stillwater-sc.com>
2020-06-19 14:58:25 -0400

    code hygiene directed by Codacy

--- set_env.sh
+++ set_env.sh
@@ -1,4 +1,7 @@
+#!/bin/bash
 # source this file
+
+echo "Source this file to set the built environment variables (MTL_ROOT, UNIVERSAL_ROOT, HPRBLAS_ROOT)"
 
 export MTL_ROOT=~/dev/clones/mtl4
 export UNIVERSAL_ROOT=~/dev/clones/universal
