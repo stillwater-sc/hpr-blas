0cc74fd0464de529af02d7a6a06313e22ac22572
James Quinlan <jquinlan@une.edu>
2020-07-20 09:52:16 -0400

    toeplitz and hankel matrices

--- include/matpak/toeplitz.hpp
+++ include/matpak/toeplitz.hpp
@@ -1,12 +1,12 @@
 /*
 // toeplitz.hpp : Generate Toeplitz matrix
+// CALL toeplitz(c,r) where c = column & r = row
 //
 // Copyright (C) 2017-2020 Stillwater Supercomputing, Inc.
 // Author: James Quinlan
 //
 // This file is part of the HPRBLAS project, which is released under an MIT Open Source license.
 */
-#include <vector>
 
 namespace sw { namespace hprblas { namespace matpak {
 
@@ -15,8 +15,24 @@ mtl::dense2D<typename Vector::value_type> toeplitz(const Vector&c, const Vector&
     typedef typename Vector::value_type value_type;
 
     using Matrix = mtl::dense2D<value_type>;
+ 
+    int m = num_rows(c);
+    int n = num_rows(r);
 
-    Matrix A(4,4);
+    Matrix A(m,n);
+
+    for(size_t i = 0; i< m; ++i){
+        for(size_t j = 0; j< n; ++j){
+            if(i==j){
+                A[i][j]=r[0];
+            }else if(i < j){
+                A[i][j] = r[j - i ];
+            }else{ // i >j
+                A[i][j] = c[i - j];
+            }
+        }
+    }
+
     return A;
 }
 }}} // namespace sw::hprblas::matpak
