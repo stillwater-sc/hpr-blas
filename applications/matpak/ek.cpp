95b22f77edbb0d263f5e59a8aeb4a3241a8edc88
James Quinlan <jquinlan@une.edu>
2020-07-23 17:03:46 -0400

    adding standard basis vector generator

---
+++ applications/matpak/ek.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <hprblas>
+#include <matpak/ek.hpp>
+
+
+int main ()
+{
+	using namespace std;
+	using namespace mtl;
+	using namespace sw::unum;
+	using namespace sw::hprblas;
+	using namespace sw::hprblas::matpak;
+	cout << setprecision(5);
+	{
+		using Scalar = posit<16,1>;
+		// using Scalar = double;
+		auto v = ek<Scalar>(3,8);
+		std::cout <<  v << std::endl;
+ 	}
+
+
+	return 0;
+}
---
+++ include/matpak/ek.hpp
@@ -0,0 +1,19 @@
+#pragma once
+// ek.hpp : i-th standard basis vector of length n
+//
+// Copyright (C) 2017-2020 Stillwater Supercomputing, Inc.
+// Author: James Quinlan
+//
+// This file is part of the HPRBLAS project, which is released under an MIT Open Source license.
+
+#include<universal/posit/posit_fwd.hpp>
+namespace sw { namespace hprblas { namespace matpak {
+
+template<typename Scalar>
+mtl::vec::dense_vector<Scalar> ek(size_t i, size_t n ) {
+    mtl::vec::dense_vector<Scalar> v(n, Scalar(0));
+	v[i]=1;
+    return v;
+}
+
+}}} // namespace sw::hprblas::matpak
