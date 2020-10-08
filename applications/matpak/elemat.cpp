0c3419301dcabbbe92b3290e43e049958fa68311
James Quinlan <jquinlan@une.edu>
2020-10-08 13:36:33 -0400

    Initial Addition

---
+++ applications/matpak/elemat.cpp
@@ -0,0 +1,52 @@
+// elemat.cpp : Elementary Matrix
+//
+// Copyright (C) 2017-2020 Stillwater Supercomputing, Inc.
+// Author: James Quinlan
+//
+// This file is part of the HPRBLAS project, which is released under an MIT Open Source license.
+
+#include <iostream>
+#include <hprblas>
+#include <matpak/elemat.hpp>
+
+// Selects posits or floats
+#define USE_POSIT 1
+
+int main ()
+{
+	using namespace std;
+	using namespace mtl;
+	using namespace sw::unum;
+	using namespace sw::hprblas;
+	using namespace sw::hprblas::matpak;
+	
+#if USE_POSIT
+    constexpr size_t nbits = 16;
+	constexpr size_t es = 1;
+	using Scalar = posit<nbits, es>;
+	cout << "\nUsing POSIT<" << nbits << "," <<  es << ">\n" <<  endl;
+#else	  
+	using Scalar = double;
+#endif
+// --------------------------------------------- //
+		
+	using Matrix = mtl::mat::dense2D< Scalar >;
+	Matrix A = elemat< Matrix >(8,3,4,2);   //
+    cout << setprecision(5);
+	std::cout <<  A << std::endl;
+
+     Matrix B = {
+				{1, 2, 3},
+				{4, 5, 6},
+				{7, 8, 9}
+			}; 
+
+        Matrix C = {
+				{1, 2, 3},
+				{4, 5, 6},
+				{7, 8, 9}
+			}; 
+        std::cout << B + C << std::endl;
+
+return 0;
+}
